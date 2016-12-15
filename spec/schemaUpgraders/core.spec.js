28575fa8abacbb167174b680fb251db0c04981f7
Kevin <kevin@algn.uk>
2016-12-15 08:05:31 +0000

    updates to copyright text

--- spec/schemaUpgraders/core.spec.js
+++ spec/schemaUpgraders/core.spec.js
@@ -22,7 +22,7 @@
  *     * Redistributions in binary form must reproduce the above copyright
  *       notice, this list of conditions and the following disclaimer in the
  *       documentation and/or other materials provided with the distribution.
- *     * The name of the Universities of Southampton nor the name of its
+ *     * The name of the University of Southampton nor the name of its
  * 	  contributors may be used to endorse or promote products derived from
  * 	  this software without specific prior written permission.
  *
